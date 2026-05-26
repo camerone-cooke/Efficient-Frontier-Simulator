a121a4c315f604a46b5a6db22e8874b08f23a61e
Cameron Cooke <came.cooke@gmail.com>
2026-05-26 19:03:10 -0400

    Description of program updated

---
+++ Efficient_Frontier_Simulation.py
@@ -0,0 +1,11 @@
+# Cameron Cooke
+# Copyright © 2026
+
+"""
+This program utilizes historical equity returns and a benchmark tracking error
+constraint to simulate and visualize the efficient frontier. Using a Monte Carlo
+simulation, the system maps thousands of distinct portfolios with varying asset
+weight combinations to a risk-return scatter plot. This visualization displays 
+the risk-return tradoff, allowing for the identitfication of optimal position 
+allocations that maximize the return for a given level of risk.
+"""
