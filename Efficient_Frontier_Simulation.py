c9ab16a45a2711fff9aa128b09bdd411fc0139da
Cameron Cooke <came.cooke@gmail.com>
2026-05-31 09:31:33 -0400

    Updated program description

--- Efficient_Frontier_Simulation.py
+++ Efficient_Frontier_Simulation.py
@@ -2,12 +2,8 @@
 # Copyright © 2026
 
 """
-This program utilizes historical equity returns and a benchmark tracking error
-constraint to simulate and visualize the efficient frontier. Using a Monte Carlo
-simulation, the system maps thousands of distinct portfolios with varying asset
-weight combinations to a risk-return scatter plot. This visualization displays 
-the risk-return tradoff, allowing for the identitfication of optimal position 
-allocations that maximize the return for a given level of risk.
+Efficient Frontier simulator using Monte Carlo simulation and multi-asset 
+covariance for generating a modern portfolio theory analytical dashboard.
 """
 
 # import needed libraries
